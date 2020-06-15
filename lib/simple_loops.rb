4e19f48919e31c300398aa8b153f84800c57120d
Haley Ferro <haleyvferro@gmail.com>
2020-06-15 01:18:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,10 @@
-def loop_message_five_times
-  
+def loop_message_five_times(message)
+    count = 0
+  while count < 5 do
+    puts message
+    message = "Hello world!"
+    count += 1
+  end
 end
 
 
