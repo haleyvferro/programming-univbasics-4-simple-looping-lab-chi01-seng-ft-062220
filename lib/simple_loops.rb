52bbeee561fd98d3f42172f94f80f10b8316148e
Haley Ferro <haleyvferro@gmail.com>
2020-06-15 01:30:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,8 +17,12 @@ def loop_message_n_times(message, limit)
 end
 
 
-def output_array
-  
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
 end
 
 
